4772672a489f03d5949e3e6c8942a178bb66b5d1
Óscar O <oscarforjuela@hotmail.com>
2022-11-09 17:26:20 -0500

    Método Inicio Grila

--- modelojuego.js
+++ modelojuego.js
@@ -4,4 +4,14 @@ class ModeloJuego{
         this.fallingPiece = null;
         this.grid = this.makeStartingGrid();
     }
+
+    makeStartingGrid(){
+        let  grid = [];
+        for(var i = 0; i < ROWS; i++){
+            for(var j = 0; j < COLS; j++){
+                grid[grid.length - 1].push(0);
+            }
+        }
+        return grid;
+    }
 }
